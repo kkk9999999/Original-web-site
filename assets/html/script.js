543b8bd619602f278003b49e8bb4247c1aba2bc4
kkk9999999 <k.kousuke0508@icloud.com>
2025-03-04 10:37:31 +0900

    Initial commit

---
+++ assets/html/script.js
@@ -0,0 +1,169 @@
+const translations = {
+    ja: {
+        home1: "始め方",
+        home2: "おすすめ動画",
+        home3: "ゲレンデ情報",
+        home4: "ニュース",
+        home5: "レッスン申し込み",
+        hom1: "始め方",
+        hom2: "おすすめ動画",
+        hom3: "ゲレンデ情報",
+        hom4: "ニュース",
+        hom5: "レッスン申し込み",
+        Purpose: "始め方",
+        Movie: "おすすめ動画",
+        Gelande: "ゲレンデ情報",
+        News: "ニュース",
+        Lesson: "レッスン申し込み",
+        Yoyaku: "スクール検索＆レッスン予約はこちら",
+        Osusume: "～おすすめのスクール～",
+        maru1: "スキー選びに迷ってる人向け！安心の定番スキー6選",
+        m1: "誰にとっても乗りやすく、どんな場面でも扱いやすい定番モデルは、時代とともに滑りのトレンドやテクノロジー、ライダーの求めるものに合わせて進化を続け、洗練され、選ばれ続けてきたもの。最新の技術や潮流を取り入れた定番スキーは、初中級者のスキルアップをサポートしてくれる心強い存在だ。",
+        maru2: "2024~2025　オリオンツアー特集",
+        m2: "スキー＆スノボバスツアー！オリオンツアーが提供する全国１００か所以上のスキー場のツアーを取り扱い！関東近畿、長野、新潟、それぞれの人気スキー場ご紹介！",
+        maru3: "スノーボードが楽しく安全に滑れる!? スノーサーフィンレッスンキャンプ(SSLC) 体験レポート",
+        m3: "今シーズンは基本をしっかりと抑え安全にスノーボードを更に上達をしたいと考えていたSURF&SNOWスタッフ。z 安全に楽しく上達できるスノーサーフィンのレッスン「スノーサーフィンレッスンキャンプ(SSLC) 」があることを知り、早速体験してきたので、当日の様子をレポートします。",
+        maru4: "サンメドウズスキースクール【常設】",
+        m4: "サンメドウズ清里スキー場（山梨県）ビギナー大歓迎！さらに腕を上げたい方も、楽しいレッスンでレベルアップ間違いなし。スキー・スノーボードを遊びながら6つのステップで上達するプログラム「スキッズ・キャンプ」が人気です。",
+        maru5: "野沢温泉スキースクール【常設】",
+        m5: "野沢温泉スキー場（長野県）野沢温泉スキー場の広大で変化に富んだゲレンデ、コースを使用したレッスン。初めてスキー・スノーボードをされる方から上級者の方まで、お客様の技術にあわせたレッスンをご提案。個人や仲間で楽しくレベルアップしたい方や家族で楽しく滑りたい、ゲレンデ巡りをしたいというお客様には、お気に入りの先生を指名することもできるプライベートレッスンや、競技スキーコーチの派遣も行っております。雪も絶景も温泉も全て天然100％。本物の感動と楽しさがある日本最大級のビッグスノーエリア野沢温泉スキー場で、是非、最高の冬を楽しんでください！",
+        maru6: "SIA公認 乗鞍プロスキースクール【常設】",
+        m6: "Mt.乗鞍スノーリゾート（長野県）スノースポーツの上達には基本をマスターする事が大切です。NPSSでは基本から応用技術まで　分かりやすく丁寧にレッスン致します。この機会に是非入校してみてはいかがでしょうか。英語　インドネシア語　マレー語のレッスンにも対応致します宿泊をご希望の方は　宿泊先を紹介致しますので　yamakih1@icloud.comまで連絡ください。。スタッフ一同お待ちしております。「Instagram　@norikura_school　でも随時情報を発信しています！」",
+        morelook: "もっと見る→",
+
+    },
+    en: {
+        home1: "How to Start",
+        home2: "Movie",
+        home3: "Slope",
+        home4: "News",
+        home5: "Lesson&School",
+        hom1: "How to Start",
+        hom2: "Movie",
+        hom3: "Slope",
+        hom4: "News",
+        hom5: "Lesson&School",
+        Purpose: "How to Start",
+        Movie: "Movie",
+        Gelande: "Slope",
+        News: "News",
+        Lesson: "Lesson&School",
+        Yoyaku: "Click here to search for schools and reserve lessons",
+        Osusume: "～Recommended school～",
+        maru1: "For those who are confused about choosing skis! 6 reliable classic skis",
+        m1: "The standard model, which is easy for anyone to ride and handle in any situation, has continued to evolve and be refined over time to suit skiing trends, technology, and what riders want. Standard skis that incorporate the latest technology and trends are a reassuring presence that will help beginner and intermediate skiers improve their skills.",
+        maru2: "2024~2025 Orion Tour Special Feature",
+        m2: "Ski & snowboard bus tour! We handle tours to more than 100 ski resorts nationwide provided by Orion Tour! Introducing popular ski resorts in Kanto Kinki, Nagano, and Niigata!",
+        maru3: "Snowboarding can be fun and safe! Snow Surfing Lesson Camp (SSLC) Experience Report",
+        m3: "This season, the SURF & SNOW staff wanted to further improve their snowboarding by focusing on the basics and safely.I learned that there is a Snow Surfing Lesson Camp (SSLC), a snow surfing lesson where you can improve your skills in a safe and fun way, and I immediately went to try it out, so I would like to report on what happened that day.",
+        maru4: "Sun Meadows Ski School [Permanent]",
+        m4: "Sun Meadows Kiyosato Ski Resort (Yamanashi Prefecture)    Beginners welcome! For those who want to further improve their skills, these fun lessons are sure to improve your level.``Skids Camp,'' a program that helps you improve your skiing and snowboarding skills through six steps, is popular.",
+        maru5: "Nozawa Onsen Ski School [Permanent]",
+        m5: "Nozawa Onsen Ski Resort (Nagano Prefecture)     Lessons using the vast and varied slopes and courses of Nozawa Onsen Ski Resort. We offer lessons tailored to your skill level, from first-time skiers and snowboarders to advanced skiers.For those who want to improve their skiing skills individually or with friends, or who want to have fun skiing with their families or visit the slopes, we offer private lessons where you can designate your favorite teacher, and we also dispatch competitive ski coaches. The snow, the spectacular scenery, and the hot springs are all 100% natural. Please come and enjoy the best winter at Nozawa Onsen Ski Resort, Japan's largest big snow area with real excitement and fun!",
+        maru6: "SIA certified Norikura Pro Ski School [Permanent]",
+        m6: "Mt. Norikura Snow Resort (Nagano Prefecture)    It is important to master the basics to improve your snow sports.At NPSS, we provide easy-to-understand and detailed lessons from basics to applied techniques. Why not take this opportunity to enroll?We also offer lessons in English, Indonesian, and Malay.If you would like to stay overnight, please contact yamakih1@icloud.com so we can introduce you to a place to stay.All the staff are looking forward to seeing you.“We also send out information on Instagram @norikura_school from time to time!”",
+        morelook: "view more→",
+    },
+    ta: {
+        home1: "Paano Magsisimula",
+        home2: "Pelikula",
+        home3: "Slope",
+        home4: "Balita",
+        home5: "Aralin&Paaralan",
+        hom1: "Paano Magsisimula",
+        hom2: "Pelikula",
+        hom3: "Slope",
+        hom4: "Balita",
+        hom5: "Aralin&Paaralan",
+        Purpose: "Paano Magsisimula",
+        Movie: "Pelikula",
+        Gelande: "Slope",
+        News: "Balita",
+        Lesson: "Aralin&Paaralan",
+        Yoyaku: "Mag-click dito upang maghanap ng mga paaralan at magreserba ng mga aralin",
+        Osusume: "～Inirerekomendang paaralan～",
+        maru1: "Para sa mga nalilito sa pagpili ng skis! 6 maaasahang klasikong skis",
+        m1: "Ang karaniwang modelo, na madaling sakyan at hawakan ng sinuman sa anumang sitwasyon, ay patuloy na umuunlad at pinipino sa paglipas ng panahon upang umangkop sa mga uso sa skiing, teknolohiya, at kung ano ang gusto ng mga sakay. Ang mga karaniwang ski na may kasamang pinakabagong teknolohiya at mga uso ay isang nakapagpapatibay na presensya na makakatulong sa mga baguhan at intermediate na skier na mapabuti ang kanilang mga kasanayan.",
+        maru2: "2024~2025 Orion Tour Espesyal na Tampok",
+        m2: "Ski at snowboard bus tour! Pinangangasiwaan namin ang mga paglilibot sa higit sa 100 ski resort sa buong bansa na ibinigay ng Orion Tour! Ipinapakilala ang mga sikat na ski resort sa Kanto Kinki, Nagano, at Niigata!",
+        maru3: "Ang snowboarding ay maaaring maging masaya at ligtas! Ulat sa Karanasan sa Snow Surfing Lesson Camp (SSLC).",
+        m3: "Sa season na ito, gustong pahusayin pa ng staff ng SURF & SNOW ang kanilang snowboarding sa pamamagitan ng pagtutok sa mga pangunahing kaalaman at ligtas.",
+        maru4: "Sun Meadows Ski School [Permanent]",
+        m4: "Sun Meadows Kiyosato Ski Resort (Yamanashi Prefecture) Maligayang pagdating sa mga nagsisimula! Para sa mga gustong pagbutihin pa ang kanilang mga kasanayan, ang nakakatuwang mga aralin na ito ay tiyak na magpapahusay sa iyong antas.``Skids Camp,'' isang programang tumutulong sa iyong pagbutihin ang iyong mga kasanayan sa skiing at snowboarding sa pamamagitan ng anim na hakbang, ay sikat.",
+        maru5: "Nozawa Onsen Ski School [Permanent]",
+        m5: "Nozawa Onsen Ski Resort (Nagano Prefecture) Mga aralin gamit ang malawak at iba't ibang slope at kurso ng Nozawa Onsen Ski Resort. Nag-aalok kami ng mga aralin na iniayon sa antas ng iyong kakayahan, mula sa mga unang beses na skier at snowboarder hanggang sa mga advanced na skier. Para sa mga gustong pagbutihin ang kanilang mga kasanayan sa skiing nang isa-isa o kasama ang mga kaibigan, o gustong magsaya sa skiing kasama ang kanilang mga pamilya o bumisita sa mga slope, nag-aalok kami ng mga pribadong lesson kung saan maaari mong italaga ang iyong paboritong guro, at nagpapadala rin kami ng mga mapagkumpitensyang ski coach. Ang niyebe, ang nakamamanghang tanawin, at ang mga hot spring ay 100% natural. Mangyaring pumunta at tamasahin ang pinakamahusay na taglamig sa Nozawa Onsen Ski Resort, ang pinakamalaking malaking snow area sa Japan na may tunay na kaguluhan at saya!",
+        maru6: "SIA certified Norikura Pro Ski School [Permanent]",
+        m6: "Mt. Norikura Snow Resort (Nagano Prefecture) Mahalagang makabisado ang mga pangunahing kaalaman upang mapabuti ang iyong snow sports. Bakit hindi kunin ang pagkakataong ito para mag-enroll? Nag-aalok din kami ng mga aralin sa English, Indonesian, at Malay. Kung gusto mong mag-overnight, mangyaring makipag-ugnayan sa yamakih1@icloud.com para maipakilala ka namin sa isang lugar na matutuluyan. Inaasahan ka ng lahat ng staff. “Nagpapadala rin kami ng impormasyon sa Instagram @norikura_school paminsan-minsan!”",
+        morelook: "tingnan pa",
+    },
+};
+
+function changeLanguage() {
+    const lang = document.getElementById("language").value;
+    
+    document.getElementById("home1").textContent = translations[lang].home1;
+    document.getElementById("home2").textContent = translations[lang].home2;
+    document.getElementById("home3").textContent = translations[lang].home3;
+    document.getElementById("home4").textContent = translations[lang].home4;
+    document.getElementById("home5").textContent = translations[lang].home5;
+    document.getElementById("hom1").textContent = translations[lang].hom1;
+    document.getElementById("hom2").textContent = translations[lang].hom2;
+    document.getElementById("hom3").textContent = translations[lang].hom3;
+    document.getElementById("hom4").textContent = translations[lang].hom4;
+    document.getElementById("hom5").textContent = translations[lang].hom5;
+    document.getElementById("Purpose").textContent = translations[lang].Purpose;
+    document.getElementById("Movie").textContent = translations[lang].Movie;
+    document.getElementById("Gelande").textContent = translations[lang].Gelande;
+    document.getElementById("News").textContent = translations[lang].News;
+    document.getElementById("Lesson").textContent = translations[lang].Lesson;
+    document.getElementById("Yoyaku").textContent = translations[lang].Yoyaku;
+    document.getElementById("Osusume").textContent = translations[lang].Osusume;
+    document.getElementById("maru1").textContent = translations[lang].maru1;
+    document.getElementById("m1").textContent = translations[lang].m1;
+    document.getElementById("maru2").textContent = translations[lang].maru2;
+    document.getElementById("m2").textContent = translations[lang].m2;
+    document.getElementById("maru3").textContent = translations[lang].maru3;
+    document.getElementById("m3").textContent = translations[lang].m3;
+    document.getElementById("maru4").textContent = translations[lang].maru4;
+    document.getElementById("m4").textContent = translations[lang].m4;
+    document.getElementById("maru5").textContent = translations[lang].maru5; 
+    document.getElementById("m5").textContent = translations[lang].m5;
+    document.getElementById("maru6").textContent = translations[lang].maru6;
+    document.getElementById("m6").textContent = translations[lang].m6;
+    document.getElementById("morelook").textContent = translations[lang].morelook;
+}
+
+
+
+
+const imageMap = {
+    "ja": "./スクリーンショット 2025-02-27 135308.png",
+    "en": "./スクリーンショット 2025-03-01 192635.png",
+    "ta": "./スクリーンショット 2025-03-03 165834.png",
+};
+const language = document.getElementById("language");
+const displayImage = document.getElementById("displayImage");
+language.addEventListener("change", function () {
+    const selectedLanguage = this.value;
+    displayImage.src = imageMap[selectedLanguage]; 
+});
+
+
+
+
+
+
+window.onscroll = function() {
+    let button = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.display = "flex";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
